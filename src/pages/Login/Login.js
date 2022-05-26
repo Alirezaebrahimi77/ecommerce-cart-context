0a9a9e401ec79dca0801f62a7f76dcd9fba4b6f7
Alireza <alireza27163@gmail.com>
2022-05-26 16:19:15 +0300

    fixed login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import LoginForm from '../../components/Login/LoginForm'
 import Layout from '../../layout/Layout'
-import styles from "./Login.module.css"
 function Login() {
   return (
     <Layout>
